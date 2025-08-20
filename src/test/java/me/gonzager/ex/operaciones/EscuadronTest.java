a44d35a3f6cc198f8c056fc3a148b98658dfd8b9
NicolasUrdiales <nicolas.urdiales@estudiantes.unahur.edu.ar>
2025-08-20 19:26:11 -0300

    faltan test

--- src/test/java/me/gonzager/ex/operaciones/EscuadronTest.java
+++ src/test/java/me/gonzager/ex/operaciones/EscuadronTest.java
@@ -12,7 +12,7 @@ public class EscuadronTest {
 
     @Test()
     void operarZona() {
-
+        
     }
 
 }
---
+++ src/test/java/me/gonzager/ex/operaciones/drones/DronTest.java
@@ -0,0 +1,37 @@
+package me.gonzager.ex.operaciones.drones;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class DronTest {
+    @Test
+    void testDisminuirAutonomia() {
+    
+    }
+
+    @Test
+    void testEficenciaOperativa() {
+
+    }
+
+    @Test
+    void testEsAvanzado() {
+
+    }
+
+    @Test
+    void testGetAutonomia() {
+
+    }
+
+    @Test
+    void testGetNivelProcesamiento() {
+
+    }
+
+    @Test
+    void testSetMision() {
+
+    }
+}
